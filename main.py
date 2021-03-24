6fe37debcd2b89f5f4ba09b58088efcd14b1e365
Indo Yoon <freedom_zero@naver.com>
2021-03-24 15:39:54 +0900

    add response

--- main.py
+++ main.py
@@ -7,31 +7,25 @@ from youtube_client import YoutubeClient
 
 if __name__ == "__main__":
     print('Status: 200')
-    print("Content-type: application/json; utf-8\r\n\r\n")
+    print("Content-type: application/json; utf-8rnrn")
 
     fs = cgi.FieldStorage()
     video_url = fs["text"].value
-    response_url = fs["response_url"].value
+    response_url = "https://hooks.slack.com/services/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"
 
     playlist_id = "PLnqRT9qVgyIDvGJm32xds8BvKwhGJ0526"
     yc = YoutubeClient(playlist_id)
 
     code, text = yc.add_new_item_to_playlist(video_url)
-    response = {
-        "blocks": [
-            {
-                "type": "section",
-                "response_type": "in_channel",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": text,
-                },
-            },
-        ]
+    payload = {
+        "channel": "#성수_신청곡_받습니다",
+        "username": "MusicBot",
+        "text": text,
+        "icon_emoji": ":musical_note:",
     }
     requests.post(
         response_url,
-        json=response,
+        json=payload,
     )
 
     print(response)
