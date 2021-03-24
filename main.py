dad178b152807fa61c4e9a83d51d43ff258bd691
Indo Yoon <freedom_zero@naver.com>
2021-03-24 14:36:53 +0900

    add response

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 import cgi
-from urllib.parse import urlencode
-import json
 
 from youtube_client import YoutubeClient
 
 if __name__ == "__main__":
     print('Status: 200')
-    print("Content-type: application/json\n\n")
+    print("Content-type: application/json\r\n\r\n")
 
     fs = cgi.FieldStorage()
     video_url = fs["text"].value
